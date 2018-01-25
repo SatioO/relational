0aeafd80e82ca9de4d40846a5dde557fb8226e8b
Brain <vaibhav.satam29991@gmail.com>
2018-01-25 14:23:38 +0530

    fix(employee): fixed minor bugs

--- routes/employee.js
+++ routes/employee.js
@@ -15,7 +15,7 @@
 	router.get("/", (req, res) => {
 		pool.getConnection((err, connection) => {
 			connection.query(
-				`SELECT *, cities.name as Emp_City_Name, states.name as Emp_State_Name FROM employee_master INNER JOIN cities INNER JOIN states ON employee_master.Emp_City = cities.id AND employee_master.Emp_State = states.id`,
+				`SELECT *, cities.name as Emp_City_Name, states.name as Emp_State_Name FROM employee_master INNER JOIN cities INNER JOIN states ON employee_master.Emp_City = cities.id AND employee_master.Emp_State = states.id WHERE Emp_Delete_Flag = 'N'`,
 				(err, rows) => {
 					if (err) {
 						return res.status(500).json(err);
@@ -84,7 +84,7 @@
 
 		pool.getConnection((err, connection) => {
 			connection.query(
-				"UPDATE employee_master SET Emp_Title = ?, Emp_Name = ?, Emp_MName = ?, Emp_Surname = ?, Emp_Gender = ?, Emp_DOB = ?, Emp_Pic_Upload = ?, Emp_Marital_Status = ?, Emp_BloodG = ?, Emp_DOAnni = ?, Emp_DOJ = ?, Emp_DOR = ?, Emp_CCode = ?, Emp_Phone = ?, Emp_MCode = ?, Emp_Mobile = ?, Emp_AMCode = ?, Emp_AMobile = ?, Emp_Email1 = ?, Emp_Email2 = ?, Emp_Linkedin = ?, Emp_Twitter = ?, Emp_FB = ?, Emp_Add1 = ?, Emp_Country = ?, Emp_State = ?, Emp_City = ?, Emp_Pincode = ?, Emp_Add2 = ?, Emp_Country2 = ?, Emp_State2 = ?, Emp_City2 = ?, Emp_Pincode2 = ?, Emp_PAN = ?, Emp_Adhar = ?, Emp_Bank_Name = ?, Emp_Bank_AccNo = ?, Emp_IFSC_Code = ?, Emp_Pass_No = ?, Emp_Pass_Expiry = ?, Emp_Pass_Upload = ?, Emp_FC1_Name = ?, Emp_FC1_Relation = ?, Emp_FC1_Email = ?, Emp_FC2_Name = ?, Emp_FC2_Relation = ?, Emp_FC2_Email = ?, Emp_FC3_Name = ?, Emp_FC3_Relation = ?, Emp_FC3_Email = ?, Emp_FC4_Name = ?, Emp_FC4_Relation = ?,  Emp_FC4_Email = ?, Emp_FC5_Name = ?, Emp_FC5_Relation = ?, Emp_FC5_Email = ?, Emp_FC6_Name = ?, Emp_FC6_Relation = ?, Emp_FC6_Email = ?, Emp_Ref1_Name = ?, Emp_Ref1_Company = ?, Emp_Ref1_Phone = ?, Emp_Ref2_Name = ?, Emp_Ref2_Company = ?, Emp_Ref2_Phone = ?, Emp_Status = ?, UpdatedBy = ? WHERE Emp_ID = ?",
+				"UPDATE employee_master SET Emp_Title = ?, Emp_Name = ?, Emp_MName = ?, Emp_Surname = ?, Emp_Gender = ?, Emp_DOB = ?, Emp_Pic_Upload = ?, Emp_Marital_Status = ?, Emp_BloodG = ?, Emp_DOAnni = ?, Emp_DOJ = ?, Emp_DOR = ?, Emp_CCode = ?, Emp_Phone = ?, Emp_MCode = ?, Emp_Mobile = ?, Emp_AMCode = ?, Emp_AMobile = ?, Emp_Email1 = ?, Emp_Email2 = ?, Emp_Linkedin = ?, Emp_Twitter = ?, Emp_FB = ?, Emp_Add1 = ?, Emp_Country = ?, Emp_State = ?, Emp_City = ?, Emp_Pincode = ?, Emp_Add2 = ?, Emp_Country2 = ?, Emp_State2 = ?, Emp_City2 = ?, Emp_Pincode2 = ?, Emp_PAN = ?, Emp_Adhar = ?, Emp_Bank_Name = ?, Emp_Bank_AccNo = ?, Emp_IFSC_Code = ?, Emp_Pass_No = ?, Emp_Pass_Expiry = ?, Emp_Pass_Upload = ?, Emp_FC1_Name = ?, Emp_FC1_Relation = ?, Emp_FC1_Email = ?, Emp_FC2_Name = ?, Emp_FC2_Relation = ?, Emp_FC2_Email = ?, Emp_FC3_Name = ?, Emp_FC3_Relation = ?, Emp_FC3_Email = ?, Emp_FC4_Name = ?, Emp_FC4_Relation = ?,  Emp_FC4_Email = ?, Emp_FC5_Name = ?, Emp_FC5_Relation = ?, Emp_FC5_Email = ?, Emp_FC6_Name = ?, Emp_FC6_Relation = ?, Emp_FC6_Email = ?, Emp_Ref1_Name = ?, Emp_Ref1_Company = ?, Emp_Ref1_Phone = ?, Emp_Ref2_Name = ?, Emp_Ref2_Company = ?, Emp_Ref2_Phone = ?, Emp_Docs = ?, Emp_Status = ?, UpdatedBy = ? WHERE Emp_Master_No = ?",
 				[
 					employee.Emp_Title,
 					employee.Emp_Name,
@@ -151,9 +151,10 @@
 					employee.Emp_Ref2_Name,
 					employee.Emp_Ref2_Company,
 					employee.Emp_Ref2_Phone,
+					employee.Emp_Docs,
 					employee.Emp_Status,
 					employee.UpdatedBy,
-					employee.Emp_ID
+					employee.Emp_Master_No
 				],
 				(err, rows) => {
 					if (err) {
@@ -168,23 +169,45 @@
 						employee.Emp_Surname
 					}/`;
 
-					fs.readdir(srcpath, (err, files) => {
-						if (err) {
-							return res.status(500).json(err);
-						}
-
-						files.forEach(file => {
-							try {
-								fs.moveSync(srcpath + file, dstpath + file, {
-									overwrite: true
-								});
-							} catch (e) {
+					if (employee.Attached) {
+						fs.readdir(srcpath, (err, files) => {
+							if (err) {
 								return res.status(500).json(err);
 							}
-						});
 
+							files.forEach(file => {
+								try {
+									fs.moveSync(srcpath + file, dstpath + file, {
+										overwrite: true
+									});
+								} catch (e) {
+									return res.status(500).json(err);
+								}
+							});
+							fs.removeSync(srcpath);
+							return res.status(200).json(rows);
+						});
+					} else {
 						return res.status(200).json(rows);
-					});
+					}
+				}
+			);
+			connection.release();
+		});
+	});
+
+	router.delete("/", (req, res) => {
+		const id = req.query.id;
+
+		pool.getConnection((err, connection) => {
+			connection.query(
+				`UPDATE employee_master SET Emp_Delete_Flag ='Y' WHERE Emp_Master_No = ?`,
+				[id],
+				(err, result) => {
+					if (err) {
+						return res.status(500).json(err);
+					}
+					return res.status(200).json(result);
 				}
 			);
 			connection.release();
@@ -212,7 +235,7 @@
 		const q = req.query.q;
 		pool.getConnection((err, connection) => {
 			connection.query(
-				`SELECT *, cities.name as Emp_City_Name, states.name as Emp_State_Name FROM employee_master INNER JOIN cities INNER JOIN states ON employee_master.Emp_City = cities.id AND employee_master.Emp_State = states.id WHERE Emp_Name LIKE ? OR Emp_MName LIKE ? OR Emp_Surname LIKE ? OR Emp_Phone LIKE ? OR Emp_Pincode LIKE ? OR Emp_Add1 LIKE ? OR Emp_Email1 LIKE ?`,
+				`SELECT *, cities.name as Emp_City_Name, states.name as Emp_State_Name FROM employee_master INNER JOIN cities INNER JOIN states ON employee_master.Emp_City = cities.id AND employee_master.Emp_State = states.id WHERE (Emp_Name LIKE ? OR Emp_MName LIKE ? OR Emp_Surname LIKE ? OR Emp_Phone LIKE ? OR Emp_Pincode LIKE ? OR Emp_Add1 LIKE ? OR Emp_Email1 LIKE ?) AND (Emp_Delete_Flag = "N")`,
 				[`%${q}%`, `%${q}%`, `%${q}%`, `%${q}%`, `%${q}%`, `%${q}%`, `%${q}%`],
 				(err, rows) => {
 					if (err) {
